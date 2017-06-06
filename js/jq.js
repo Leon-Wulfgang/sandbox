97ae5ac4c878c98211e1136b2254ef1c755b2db3
Hao Wu <haowu0802@gmail.com>
2017-06-05 17:24:13 -0700

    jQ events
    jQ events

--- js/jq.js
+++ js/jq.js
@@ -39,12 +39,12 @@ $(function(){
   /* remove element */
   $('#rm').remove();
 
-  /* append/prepend */
+  /* append/prepend create children*/
   firstLi = $('#jqul').first();
   firstLi.prepend('<li>prepend</li>');
   firstLi.append('<li>append</li>');
 
-  /* insertBefore/After */
+  /* insertBefore/After create siblings*/
   toInsertBefore =  $('<li>liInsertBefore</li>');
   toInsertAfter =  $('<li>liInsertAfter</li>');
   toInsertBefore.insertBefore($('#testitem'));
@@ -52,7 +52,7 @@ $(function(){
   $('.classitem').after('<li>liAfterClass</li>');
   $('.classitem').before('<li>liBeforeClass</li>');
 
-  /* each */
+  /* each, modify list of ele*/
   $('li').each(function(){
     $(this).text($(this).text()+'+each');
   });
@@ -62,7 +62,46 @@ $(function(){
   //h1 = articleList.siblings('h1');     /*find all tags of h1 in siblings*/
   //articleList = $('.article-list');     /*returns jQ DOM object*/
 
+  /* monitor event */
+  /*monitorEvents() only avaialbe in Chrome dev tool
+  */
 
+  /*
+  jQ event listner
+  */
+  /* click button turns purple */
+  $('#my-button').on('click',function(e){
+    console.log(e);
+    //$(this).remove();
+    $(e.target).css('background-color','purple');
+    $('body').attr('class','success');
+  });
+  /* overwrite a-href to log and not redirect */
+  $('#fakelink').on('click',function(e){
+    e.preventDefault(); /*precent default redirect*/
+    console.log('fakelink clicked');
+  });
+  /* other events
+  event.keyCode: to learn what key was pressed
+  event.pageX/pageY: where on the page the click occurred
+  event.type: to find what event happened if listening to a target for multiple events
+  https://api.jquery.com/category/events/event-object/
+  https://api.jquery.com/event.target/
+  http://www.w3.org/TR/DOM-Level-3-Events/
+  The Convenience Method http://api.jquery.com/category/events/
+  */
+  /*event delegation
+      add event to non-exist elements
+  */
+  /* add a new li to jqul */
+  $( '#add-li' ).on( 'click', function(e) {
+    newLi = "<li>new li</li>";
+    $('#jqul').append(newLi);
+  });
+  /* add event to any li in jqul, click turns pink */
+  $( '#jqul' ).on( 'click', 'li', function(e) {
+    $(e.target).css('background-color','pink');
+  });
 
   /*
   [jQ CDN]
