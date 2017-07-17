c8d16d162de8a2905cff58632fe798d291ad191e
Hao Wu <haowu0802@gmail.com>
2017-07-16 22:07:22 -0700

    django note

---
+++ django.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+django-admin startproject --template=https://github.com/heroku/heroku-django-template/archive/master.zip --name=Procfile skillget
+#   use heroku django template to create proj
+django-admin startproject skillget  # cmd util for admin tasks
+#   startproject <name> # create dirs init files
